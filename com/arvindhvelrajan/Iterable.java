34796bfa50954b8e980475325b7b59412b3c3cb8
tun67213 <tun67213@temple.edu>
2024-07-18 12:33:55 +0900

    Updated file with generic variable

--- com/arvindhvelrajan/Iterable.java
+++ com/arvindhvelrajan/Iterable.java
@@ -1,4 +1,10 @@
 package com.arvindhvelrajan;
 
-public interface Iterable {
+/**
+ * @author arvindhvelrajan
+ * @param <T> Generic variable to run this interface on any data type
+ */
+public interface Iterable<T>
+{
+
 }
