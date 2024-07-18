1e1c20e6b73a13feb4709d97726e4e9a7054aab5
tun67213 <tun67213@temple.edu>
2024-07-18 12:54:18 +0900

    Implemented necessary function headers

--- com/arvindhvelrajan/ListIterator.java
+++ com/arvindhvelrajan/ListIterator.java
@@ -6,5 +6,61 @@ package com.arvindhvelrajan;
  */
 public interface ListIterator<E> extends Iterator<E>
 {
+    /**
+     * @param e value to be added into the list
+     * @throws NullPointerException if no value is provided
+     * @code Inserts the specified element into this list
+     */
+    void add(E e);
 
+    /**
+     * @return true if this list iterator has more elements when traversing in the
+     *         forward direction, false otherwise
+     */
+    @Override
+    boolean hasNext();
+
+    /**
+     * @return true if this list iterator has more elements when traversing in the
+     *         reverse direction, false otherwise
+     */
+    boolean hasPrevious();
+
+    /**
+     * @return the next element in the list
+     * @code Returns the next element in the list, and moves the pointer to the next value
+     */
+    @Override
+    E next();
+
+    /**
+     * @return the index of the element to be returned by a subsequent call to next()
+     */
+    int nextIndex();
+
+    /**
+     * @return the previous element in the list
+     * @code Returns the previous element in the list, and moves the pointer to the previous
+     *       value
+     */
+    E previous();
+
+    /**
+     * @return the index of the element to be returned by a subsequent call to previous()
+     */
+    int previousIndex();
+
+    /**
+     * @code Removes from the list the last element returned by next() or previous()
+     */
+    @Override
+    void remove();
+
+    /**
+     * @param e the value to replace the old one with at the pointer
+     * @throws NullPointerException if no replacement value is provided
+     * @code Replaces the last element returned by next() or previous() with the specified
+     *       element
+     */
+    void set(E e);
 }
